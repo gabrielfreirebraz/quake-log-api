c300b51ae64ef32948e57cf9441cbc8732e94f71
Gabriel Braz <gabrielfreirebraz@gmail.com>
2024-04-27 00:35:31 +0000

    feat: setup app

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from 'express'
+
+const app = express();
+
+
+app.get('/', (request: Request, response: Response) => {
+
+    response.status(200).send({message: 'ok'})
+});
+
+app.listen(3000, () => {
+
+    console.log('api running');
+});
