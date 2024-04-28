b50052755367a451486e98759616f7876c12964c
Gabriel Braz <gabrielfreirebraz@gmail.com>
2024-04-28 11:40:30 +0000

    feat: add report array and keynames for each game match

--- src/models/quakeLogModel.ts
+++ src/models/quakeLogModel.ts
@@ -56,12 +56,10 @@ export class QuakeLogModel {
 
             // Manipula o evento 'line', que é acionado quando uma nova linha é lida
             let lineNumber = 0; // Initialize line number counter
-            //let matchGameNumber = 1;
-            //var fistLine: string | null | string[] = null;
 
 
-            const arrReport: any[][] = [];
-            let arrMatchGame: any = [];
+            const matchHistoryArray: any[][] = [];
+            let eachMatchArray: any = [];
             
             leitor.on('line', (row) => {
 
@@ -72,30 +70,68 @@ export class QuakeLogModel {
                     const eventName = arrRow[1]
 
                     if (eventName === 'ShutdownGame:') {
-                        arrReport.push(arrMatchGame)
-                        arrMatchGame = [];
-                        //console.log('finish game '+matchGameNumber)
-                        //matchGameNumber++;
+                        matchHistoryArray.push(eachMatchArray)
+                        eachMatchArray = [];
                     } 
                     else if (eventName !== 'InitGame:') {
 
-                        arrMatchGame.push(row);
-                    }    
-                    
-                    //fistLine = arrRow; 
+                        eachMatchArray.push(row);      
+                    }                        
                 //}
-
-                //console.log(lineNumber)
-                //console.log('Linha lida:', line);
-
+                //console.log('Linha lida:', row);
                 lineNumber++;
             });
             
             // Manipula o evento 'close', que é acionado quando a leitura do arquivo é concluída
             leitor.on('close', () => {
                 console.log('Leitura do arquivo concluída.');
-                console.log(arrReport)
+                //console.log(matchHistoryArray)
 
+                let gameReportArray = [];
+
+                for (let i = 0; i < matchHistoryArray.length; i++) {
+                    
+                    
+                    
+                    const sessionLogsArray = matchHistoryArray[i];
+
+                    gameReportArray.push({[`game_${i+1}`]: sessionLogsArray})
+
+                    sessionLogsArray.reduce((prevValue, currValue, currIdx) => {
+                        //console.log(currValue)
+
+                        const arr_row = currValue.split(' ').filter(Boolean) // split without empty or undefined elements
+                        const eventName = arr_row[1].substring(0, arr_row[1].length - 1)
+
+
+                        //console.log(eventName);
+
+                        return prevValue + 1;
+                    },0)
+
+
+
+                    console.log('key here: '+i)
+                }
+                console.log(gameReportArray)
+
+                // matchHistoryArray.reduce((previousValue, currentValue, currentIndex) => {
+
+                //     const row = currentValue[0];
+                //     console.log(row)
+                //     return previousValue + 1;
+                // },0);
+
+                        // arrMatchGame.push(
+                        //     {
+                        //         "total_kills": 45, 
+                        //         "players": ["Dono da bola", "Isgalamido", "Zeh"],
+                        //         "kills": {
+                        //             "Dono da bola": 5,
+                        //             "Isgalamido": 18,
+                        //             "Zeh": 20
+                        //         }
+                        //     });
             });
 
             leitor.on('error', (err) => {
