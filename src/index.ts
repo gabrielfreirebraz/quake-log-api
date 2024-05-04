74fb39d49d87ce7dd00875626f99e669566b7402
Gabriel Braz <gabrielfreirebraz@gmail.com>
2024-05-04 09:47:51 +0000

    feat: add rate limit

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import express from 'express'
 import cors from 'cors'
 import helmet from "helmet";
 import { quakeLogRouter } from './routes/quakeLogRouter';
+import { limiter } from './utils/limiter';
 
 const app = express();
 
+app.use(limiter);
 app.use(helmet());
 app.use(express.json());
 app.use(cors());
