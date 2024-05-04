8ac49dcefe54405649aca03e178d59433c644702
Gabriel Braz <gabrielfreirebraz@gmail.com>
2024-05-04 09:38:11 +0000

    feat: add helmet

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import cors from 'cors'
+import helmet from "helmet";
 import { quakeLogRouter } from './routes/quakeLogRouter';
 
 const app = express();
 
-app.use(express.json())
+app.use(helmet());
+app.use(express.json());
 app.use(cors());
 app.use("/api", quakeLogRouter);
 
